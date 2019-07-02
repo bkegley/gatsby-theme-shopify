893c4d2be8b7c526053586967ed22061be29a7fc
Bryan Kegley <hi@bryankegley.me>
2019-07-02 12:36:14 -0500

    add login and logout to CustomerProvider

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -3,12 +3,38 @@ import {useCart, useCustomer} from 'gatsby-theme-shopify'
 
 const IndexPage = () => {
   const cart = useCart()
-  console.log({cart})
-  const customer = useCustomer()
-  console.log({customer})
+  const {customer, login, logout} = useCustomer()
+
+  const [values, setValues] = React.useState({})
+  const handleChange = e => {
+    setValues({...values, [e.currentTarget.name]: e.currentTarget.value})
+  }
   return (
     <div>
       <h2>This is the home page</h2>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          login(values)
+        }}
+      >
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div>
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
+      </div>
     </div>
   )
 }
--- packages/gatsby-theme-shopify/src/context/CustomerProvider.js
+++ packages/gatsby-theme-shopify/src/context/CustomerProvider.js
@@ -2,14 +2,60 @@ import React from 'react'
 
 export const CustomerContext = React.createContext()
 
+const fetchShopifyStorefront = ({shopName, storefrontAccessToken, query, api = '2019-07'}) => {
+  const url = `https://${shopName}.myshopify.com/api/graphql`
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/graphql',
+      'X-Shopify-Storefront-Access-Token': storefrontAccessToken,
+    },
+    body: query,
+  })
+}
+
+const buildLoginMutation = ({email, password}) => `
+  mutation {
+    customerAccessTokenCreate(input: {email: "${email}", password: "${password}"}) {
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+`
+
+const buildLogoutMutation = ({customerAccessToken}) => `
+  mutation {
+    customerAccessTokenDelete(customerAccessToken: "${customerAccessToken}") {
+      deletedAccessToken
+      deletedCustomerAccessTokenId
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`
+
 const LOGIN = 'LOGIN'
 const LOGOUT = 'LOGOUT'
 
-const customerReducer = (cart, action) => {
+const customerReducer = (state, action) => {
   switch (action.type) {
     case LOGIN: {
+      return {
+        ...state,
+        ...action.customerAccessTokenCreate,
+      }
     }
     case LOGOUT: {
+      return {}
     }
     default: {
       throw new Error(`Provide a valid action type`)
@@ -29,11 +75,33 @@ const CustomerProvider = ({children, shopName, storefrontAccessToken}) => {
     window.localStorage.setItem('customer', JSON.stringify(customer))
   }, [customer])
 
-  const login = ({username, password}) => {
-    dispatch({type: LOGIN, username, password})
+  const login = async ({email, password} = {}) => {
+    if (!email || !password) {
+      throw new Error(`email and password are required`)
+    }
+
+    const {data} = await fetchShopifyStorefront({
+      shopName: shopName,
+      storefrontAccessToken: storefrontAccessToken,
+      query: buildLoginMutation({email: email, password: password}),
+    }).then(res => res.json())
+
+    dispatch({type: LOGIN, customerAccessTokenCreate: data.customerAccessTokenCreate})
   }
 
-  const logout = () => {
+  const logout = async () => {
+    if (!customer.customerAccessToken || !customer.customerAccessToken.accessToken) {
+      throw new Error(`User must be logged in before logging out`)
+    }
+    const {data} = await fetchShopifyStorefront({
+      shopName: shopName,
+      storefrontAccessToken: storefrontAccessToken,
+      query: buildLogoutMutation({customerAccessToken: customer.customerAccessToken.accessToken}),
+    }).then(res => res.json())
+
+    if (data.customerAccessTokenDelete.userErrors && data.customerAccessTokenDelete.userErrors.length) {
+      throw new Error(`There was an issue logging the user out`)
+    }
     dispatch({type: LOGOUT})
   }
 
