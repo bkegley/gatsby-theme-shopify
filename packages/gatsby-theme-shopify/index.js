1d72abf5b3b3e0624bd7f9e232f499b0d8a53119
Bryan Kegley <hi@bryankegley.me>
2019-08-05 00:07:09 -0500

    exports

--- packages/gatsby-theme-shopify/index.js
+++ packages/gatsby-theme-shopify/index.js
@@ -2,6 +2,8 @@
 
 export {default as useCart} from './src/context/useCart'
 export {default as useCustomer} from './src/context/useCustomer'
+export {default as useStorefront} from './src/context/useStorefront'
 export {default as CartProvider} from './src/context/CartProvider'
 export {default as CustomerProvider} from './src/context/CustomerProvider'
 export {default as ShopifyProvider} from './src/context/ShopifyProvider'
+export {default as StorefrontProvider} from './src/context/StorefrontProvider'
