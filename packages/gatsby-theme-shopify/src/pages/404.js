bed11df2f61b196df14424c5d33b179cc1b79595
Bryan Kegley <hi@bryankegley.me>
2019-08-28 15:26:08 -0500

    add 404

---
+++ packages/gatsby-theme-shopify/src/pages/404.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import {jsx} from 'theme-ui'
+import Layout from '../components/Layout'
+
+const FourOhFourPage = () => {
+  return (
+    <Layout>
+      <div>Page not found!</div>
+    </Layout>
+  )
+}
+
+export default FourOhFourPage
